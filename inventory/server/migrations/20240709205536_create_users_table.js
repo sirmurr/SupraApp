abc9012f3c7cb7b8b1650b121025666372c70ea8
kevin <kevin.murray1116@gmail.com>
2024-07-09 17:12:51 -0400

    created exports up and down for users and items tables. Made seed files

---
+++ inventory/server/migrations/20240709205536_create_users_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.string("firstname", 250);
+    table.string("lastname", 250);
+    table.string("username", 250);
+    table.string("password", 250);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
+};
---
+++ inventory/server/src/app.js
@@ -0,0 +1,5 @@
+//const knex = require('knex')( require('./knexfile')[process.env.NODE_ENV || 'development']);
+
+const express = require("express");
+const app = express();
+const port = 8081;
