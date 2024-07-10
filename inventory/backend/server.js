e29df30a158bd646d2e014dceca68a4ddc7af2e7
kevin <kevin.murray1116@gmail.com>
2024-07-10 13:28:02 -0400

    added test-db route

--- inventory/backend/server.js
+++ inventory/backend/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const pool = require("./db");
 const port = 4000;
+const knex = require("knex")(
+  require("./knexfile.js")[process.env.NODE_ENV || "development"]
+);
 
 const app = express();
 app.use(express.json());
+
+// test db route
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await knex.raw("SELECT 1+1 AS result");
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(500).json({ message: "Database connection failed", error: err });
+  }
+});
 
 //routes
 app.get("/", async (req, res) => {
