784c349388d57268b2cdb5684a743e7464a6213d
kevin <kevin.murray1116@gmail.com>
2024-07-11 17:31:22 -0400

    Delete User Works

--- inventory/src/Components/Inventory/FilterComp/DeleteUser.js
+++ inventory/src/Components/Inventory/FilterComp/DeleteUser.js
@@ -8,10 +8,47 @@ function DeleteUser(props) {
   const [lastname, setLastname] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [id, setId] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevents the default form submission behavior
+
+    try {
+      const response = await fetch(`http://localhost:13000/users/${id}`, {
+        method: "DELETE",
+      });
+
+      setId("");
+      fetchAll();
+
+      if (response.ok) {
+        // Handle successful response
+        const data = await response.json();
+        console.log("User delete successfully:", data);
+      } else {
+        // Handle error response
+        console.error("Failed to delete user:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
-    <div>
-      <h4>DeleteUser</h4>
+    <div className="edit-form">
+      <h2>Edit User</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="id">Id:</label>
+          <input
+            type="number"
+            id="id"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add User</button>
+      </form>
     </div>
   );
 }
